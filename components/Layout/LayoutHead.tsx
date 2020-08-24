41ae7e88479d0e5684d437ebb0561d28e0b722a7
schriker <janekmachine@gmail.com>
2020-08-24 13:43:37 +0200

    Favicon

--- components/Layout/LayoutHead.tsx
+++ components/Layout/LayoutHead.tsx
@@ -27,8 +27,30 @@ const LayoutHead = ({ title, ogImage, ogDescription }: LayoutHeadProps) => {
           href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
         <script src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131295801-1"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-131295801-1"
+        ></script>
       </Head>
     </>
   );
