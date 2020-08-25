940adf8438da9d45ab99a27a25eae35b9463d186
schriker <janekmachine@gmail.com>
2020-08-25 08:33:53 +0200

    Thumbnail placeholder changed

--- components/Videos/VideoImagePlaceholder.tsx
+++ components/Videos/VideoImagePlaceholder.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const VideoImagePlaceholder = () => {
-  return <img src="/placeholder.svg" alt="" />;
+  return (
+    <svg
+      width="640"
+      height="360"
+      viewBox="0 0 640 360"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="640" height="360" fill="#232323" />
+    </svg>
+  );
 };
 
 export default VideoImagePlaceholder;
