a44198caff548a65ae9b5151e1e1ebf091e1d4f1
schriker <janekmachine@gmail.com>
2020-09-27 10:38:14 +0200

    Logs removed from worker

--- helpers/message.worker.js
+++ helpers/message.worker.js
@@ -38,7 +38,6 @@ onmessage = ({ data }) => {
 
   switch (data.type) {
     case 'START':
-      console.log(data)
       time =
         new Date(data.video.started).getTime() + (data.playerPosition * 1000) + (data.chatAdjustment * 1000);
       clearTimeout(timeInterval);
