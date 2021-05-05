348112ca15a0f98ba6d548d6445dbc35e0020fe2
schriker <janekmachine@gmail.com>
2021-05-05 13:22:30 +0200

    Worker undefined in build with webpack 5 fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-// const WorkerPlugin = require('worker-plugin');
-
 module.exports = {
   future: {
     webpack5: true,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.output.chunkFilename = isServer
+      ? `${dev ? '[name]' : '[name].[fullhash]'}.js`
+      : `static/chunks/${dev ? '[name]' : '[name].[fullhash]'}.js`;
     config.module.rules.push({
       test: /react-spring/,
       sideEffects: true,
