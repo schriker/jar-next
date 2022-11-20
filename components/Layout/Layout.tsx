ee0f35237c4c127eca1e7ae1b7b80bc34e65c923
Marcin Janus <janekmachine@gmail.com>
2022-11-20 09:05:21 +0100

    Remove donate modal

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { LayoutProps } from 'types/layout';
 import LayoutHead from 'components/Layout/LayoutHead';
 import styles from 'components/Layout/Layout.module.css';
-import Donate from 'components/Donate/Donate';
 
 const Layout = ({
   title,
@@ -12,7 +11,6 @@ const Layout = ({
 }: LayoutProps) => {
   return (
     <div className={styles.layout}>
-      <Donate />
       <LayoutHead
         title={title}
         ogImage={ogImage}
